331ea42f3f1dc9099dc3c86130c88fb09f706b05
Erica <erica@Ericas-Mac-mini.local>
2016-02-14 22:57:26 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+def countdown(count)
+    x = count
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+def countdown_with_sleep(sec)
+    sleep(sec)
 end
